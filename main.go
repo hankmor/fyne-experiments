5e8f1f275d91dc92044e72f9d69835227e4d9639
hank <hankmo.x@gmail.com>
2025-07-09 10:04:36 +0000

    feat: basic demo

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -11,7 +16,22 @@ func main() {
 	// create window to show sth.
 	w := ap.NewWindow("Hello fyne")
 	// create a label as content of window
-	w.SetContent(widget.NewLabel("This is a fyne demo!"))
+	l := widget.NewLabel("This is a fyne demo!")
+	// new update button
+	b := widget.NewButton("Update", func() {
+		tm := time.Now().Format("Time: 03:04:05")
+		l.SetText(tm)
+	})
+	// add label and button to window
+	w.SetContent(container.NewVBox(l, b))
+	// window size
+	w.Resize(fyne.NewSize(200, 100))
 	// show window and run the app
 	w.ShowAndRun()
+	// hook method after exited
+	tidyup()
+}
+
+func tidyup() {
+	fmt.Println("exited")
 }
